f0219cf7ec60aa49d15a66473818015c2056842c
Edward <yurest3it@gmai;.com>
2026-01-03 09:22:30 +0200

    TL-12: Formated with Prettier

--- tests/auth-order-without-client.spec.ts
+++ tests/auth-order-without-client.spec.ts
@@ -67,7 +67,6 @@ test('Auth and delete order by id without api client', async ({ request }) => {
   })
   expect(deleteOrder.status()).toBe(StatusCodes.OK)
 
-
   const getAfterDelete = await request.get(`${BASE_URL}/orders/${created.id}`, {
     headers: { Authorization: `Bearer ${jwt}` },
   })
